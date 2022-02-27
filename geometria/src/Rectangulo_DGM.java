724f11d6b42ba66d3e57348f9505818f9a3c9866
Daniel Garcia Martinez <dangarmar4@alu.edu.gva.es>
2022-02-27 19:55:07 +0100

    versión comentada

--- geometria/src/Rectangulo_DGM.java
+++ geometria/src/Rectangulo_DGM.java
@@ -1,8 +1,19 @@
 //Versión inicial
+/**
+ * 
+ * @author Daniel García Martínez
+ * @version 1.2
+ */
 public class Rectangulo_DGM extends FiguraGeometrica_DGM {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * 
+	 * @param tipoFigura
+	 * @param lG
+	 * @param lP
+	 */
 	public Rectangulo_DGM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
@@ -10,11 +21,19 @@ public class Rectangulo_DGM extends FiguraGeometrica_DGM {
 	}
 
 	@Override
+	/**
+	 * Método que devuelve el area
+	 * @return area
+	 */
 	public double area() {
 		return l1 * l2;
 	}
 	
 	@Override
+	/**
+	 * Método que devuelve el perimetro
+	 * @return perimetro
+	 */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
